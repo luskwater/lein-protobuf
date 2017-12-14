15501d12520c717bb501ded4554a2946ca6f3a15
Ron Lusk <rlusk@chariotsolutions.com>
2017-12-14 14:07:06 -0500

    Updated libraries used by the project
    Fixed imports; tagged use of Java IO constructs

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-protobuf "0.5.0"
+(defproject lein-protobuf "0.5.1-SNAPSHOT"
   :description "Leiningen plugin for compiling protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/ninjudd/protobuf"
-  :dependencies [[fs "1.2.0"]
-                 [conch "0.2.0"]
+  :dependencies [[fs "1.3.3"]
+                 [me.raynes/conch "0.8.0"]
                  [leinjacker "0.4.2"]]
   :eval-in-leiningen true
   ;; Bug in the current 1.x branch of Leiningen causes
--- src/leiningen/protobuf.clj
+++ src/leiningen/protobuf.clj
@@ -4,36 +4,66 @@
         [leinjacker.eval :only [in-project]]
         [leiningen.core.user :only [leiningen-home]]
         [leiningen.core.main :only [abort]])
+  (:import
+   (java.io File)
+   (java.net URL))
   (:require [clojure.java.io :as io]
             [fs.core :as fs]
             [fs.compression :as fs-zip]
-            [conch.core :as sh]))
+            [me.raynes.conch.low-level :as sh]))
 
-(def cache (io/file (leiningen-home) "cache" "lein-protobuf"))
-(def default-version "2.6.1")
+(set! *warn-on-reflection* true)
 
-(defn version [project]
+(def ^File cache
+  "Protobuf-specific cache directory for assorted uses"
+  (io/file (leiningen-home) "cache" "lein-protobuf"))
+
+(def default-proto-path
+  "Default location for project's `.proto` files"
+  "resources/proto")
+
+(def default-version
+  "Default version of protobuf kit if `:protobuf-version` is not specified."
+  "2.6.1")
+
+(defn version
+  "Version of Google Protocol Compiler to use
+
+  If `:protobuf-version` is not in project, defaults to `default-version`."
+  [project]
   (or (:protobuf-version project) default-version))
 
-(defn zipfile [project]
+(defn ^File zipfile
+  "Where to put/find the downloaded ZIP file containing the selected
+  version of the Protocol Compiler"
+  [project]
   (io/file cache (format "protobuf-%s.zip" (version project))))
 
-(defn srcdir [project]
+(defn ^File srcdir
+  "Directory under cache for storing sources"
+  [project]
   (io/file cache (str "protobuf-" (version project))))
 
-(defn protoc [project]
-  (io/file (srcdir project) "src" "protoc"))
+(defn ^File protoc
+  "Identifies `protoc` executable"
+  [project]
+  (if (:protoc project)
+    (io/file (:protoc project))
+    (io/file (srcdir project) "src" "protoc")))
 
-(defn url [project]
-  (java.net.URL.
-   (format "https://github.com/google/protobuf/releases/download/v%s/protobuf-%s.zip"
-     (version project)
+(defn ^URL url
+  "Web-source for Protocol Compiler to be downloaded"
+  [project]
+  (URL.
+   (format "https://github.com/google/protobuf/releases/download/v%1$s/protobuf-%1$s.zip"
      (version project))))
 
-(defn proto-path [project]
-  (io/file (get project :proto-path "resources/proto")))
+(defn proto-path
+  "Where the project's proto files are stored."
+  [project]
+  (io/file (get project :proto-path default-proto-path)))
 
-(def ^{:dynamic true} *compile-protobuf?* true)
+(def ^:dynamic *compile-protobuf?* true)
 
 (defn target [project]
   (doto (io/file (:target-path project))
@@ -41,7 +71,7 @@
 
 (defn extract-dependencies
   "Extract all files proto depends on into dest."
-  [project proto-path protos dest]
+  [project ^File proto-path protos ^File dest]
   (in-project (dissoc project :prep-tasks)
     [proto-path (.getPath proto-path)
      dest (.getPath dest)
@@ -63,7 +93,12 @@
                     (io/copy (io/reader resource) proto-file))
                   (recur (concat deps (dependencies proto-file)))))))))))
 
-(defn modtime [f]
+(defn modtime
+  "Return the last modification time of a file or directory.
+
+  If a single file, its mod-time; if a directory, the most recent of
+  the mod-times"
+  [f]
   (let [files (if (fs/directory? f)
                 (->> f io/file file-seq rest)
                 [f])]
@@ -71,18 +106,27 @@
       0
       (apply max (map fs/mod-time files)))))
 
-(defn proto-file? [file]
-  (let [name (.getName file)]
+(defn proto-file?
+  "Identify a `.proto` file by extension.
+
+  **Note:** the file can't be hidden, either."
+  [^File file]
+  (let [^String name (.getName file)]
     (and (.endsWith name ".proto")
          (not (.startsWith name ".")))))
 
-(defn proto-files [dir]
-  (for [file (rest (file-seq dir)) :when (proto-file? file)]
+(defn proto-files
+  "Extract the name (and relative path) of `.proto` files below `dir`."
+  [^File dir]
+  (for [^File file (rest (file-seq dir)) :when (proto-file? file)]
     (.substring (.getPath file) (inc (count (.getPath dir))))))
 
 (defn fetch
   "Fetch protocol-buffer source and unzip it."
   [project]
+  (println "************************************************************")
+  (println "**** (fetch project) ***************************************")
+  (println "************************************************************")
   (let [zipfile (zipfile project)
         srcdir  (srcdir project)]
     (when-not (.exists zipfile)
@@ -98,7 +142,9 @@
   "Compile protoc from source."
   [project]
   (let [srcdir (srcdir project)
-        protoc (protoc project)]
+        _ (println "srcdir: " srcdir)
+        protoc (protoc project)
+        _ (println "protoc: " protoc)]
     (when-not (.exists protoc)
       (fetch project)
       (fs/chmod "+x" (io/file srcdir "autogen.sh"))
@@ -109,7 +155,7 @@
       (println "Running 'make'")
       (sh/stream-to-out (sh/proc "make" :dir srcdir) :out))))
 
-(defn protoc [project]
+#_(defn protoc [project]
   "Get :protoc argument from project."
   (:protoc project))
 
@@ -117,32 +163,39 @@
   "Create .java and .class files from the provided .proto files."
   ([project protos]
      (compile-protobuf project protos (io/file (target project) "protosrc")))
-  ([project protos dest]
-     (let [target     (target project)
-           class-dest (io/file target "classes")
-           proto-dest (io/file target "proto")
-           proto-path (proto-path project)
-           protoc (or (protoc project) proto-path)]
-       (when (or (> (modtime proto-path) (modtime dest))
-                 (> (modtime proto-path) (modtime class-dest)))
-         (binding [*compile-protobuf?* false]
-           (fs/mkdirs target)
-           (fs/mkdirs class-dest)
-           (fs/mkdirs proto-dest)
-           (.mkdirs dest)
-           (extract-dependencies project proto-path protos proto-dest)
-           (doseq [proto protos]
-             (let [args (into [protoc proto
-                               (str "--java_out=" (.getAbsoluteFile dest)) "-I."]
-                              (map #(str "-I" (.getAbsoluteFile %))
-                                   [proto-dest proto-path]))]
-               (println " > " (join " " args))
-               (let [result (apply sh/proc (concat args [:dir proto-path]))]
-                 (when-not (= (sh/exit-code result) 0)
-                   (abort "ERROR:" (sh/stream-to-string result :err))))))
-           (javac (-> project
-                      (update-in [:java-source-paths] concat [(.getPath dest)])
-                      (update-in [:javac-options] concat ["-Xlint:none"]))))))))
+  ([project protos ^File dest]
+   (println "************************************************************")
+   (println "******** (compile-protobuf project " protos "  " dest "*****")
+   (println "************************************************************")
+
+   (let [target     (target project)
+         ^File class-dest (io/file target "classes")
+         ^File proto-dest (io/file target "proto")
+         proto-path (proto-path project)
+         protoc (or (protoc project) proto-path)]
+     (when (or (> (modtime proto-path) (modtime dest))
+               (> (modtime proto-path) (modtime class-dest)))
+       (binding [*compile-protobuf?* false]
+         (fs/mkdirs target)
+         (fs/mkdirs class-dest)
+         (fs/mkdirs proto-dest)
+         (.mkdirs dest)
+         (extract-dependencies project proto-path protos proto-dest)
+         (doseq [proto protos]
+           (let [args (into [] (concat  [(str protoc) proto
+                                         (str "--java_out=" (.getAbsoluteFile dest)) "-I."]
+                                        (map (fn [^File f]
+                                               (str "-I" (.getAbsoluteFile f)))
+                                             [proto-dest proto-path])))]
+             (println " > " (join " " args))
+             (let [all-the-args (concat args [:dir (str proto-path)])
+                   _ (println "all-the-args> " all-the-args)
+                   result (apply sh/proc all-the-args)]
+               (when-not (= (sh/exit-code result) 0)
+                 (abort "ERROR:" (sh/stream-to-string result :err))))))
+         (javac (-> project
+                    (update-in [:java-source-paths] concat [(.getPath dest)])
+                    (update-in [:javac-options] concat ["-Xlint:none"]))))))))
 
 (defn compile-google-protobuf
   "Compile com.google.protobuf.*"
@@ -164,7 +217,7 @@
   (let [files (or (seq files)
                   (proto-files (proto-path project)))
         protoc (protoc project)]
-    (when-not protoc
+    (when-not (.exists protoc)
       (build-protoc project))
     (when (and (= "protobuf" (:name project)))
       (compile-google-protobuf project))
